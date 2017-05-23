b51c056b9ec4aab8e60d960bb47692a32f705cec
canlas1 <dc@uscr.com>
2017-05-23 00:20:04 -0400

    push again

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -2,46 +2,43 @@
 
  var router = express.Router();
 
-var db = require("../models");
+ var db = require("../models");
 
-//Create all our routes and set up logic within those routes where required.
+ //Create all our routes and set up logic within those routes where required.
 
-router.get("/", function(req, res) {
-   db.burger.findAll({}).then(function(data) {
-  
-     var hbsObject = { burgers: data };
-    res.render('index', hbsObject);
+ router.get("/", function(req, res) {
+     db.burger.findAll({}).then(function(data) {
 
-    });
-});
+         var hbsObject = { burgers: data };
+         res.render('index', hbsObject);
 
-router.post("/burger/create", function(req, res) {
-  db.burger.create({
-    burger_name: req.body.burger_name, 
-    
-  }).then(function(dbBurger) {
-    console.log(dbBurger);
-    res.redirect('/');
-  });
-});
+     });
+ });
 
+ router.post("/burger/create", function(req, res) {
+     db.burger.create({
+         burger_name: req.body.burger_name,
 
- router.put("/burgers/update", function(req, res) {
-  console.log("Event is being updated line 29");
-   db.burger.update({
-     devoured: true
-   }, 
-     {
-      where: {
-        id: req.body.burger_id
-      }
-   }
-   ).then(function (event) {
-    console.log(event);
-     res.redirect('/');
-   });
+     }).then(function(dbBurger) {
+
+         res.redirect('/');
+     });
  });
 
 
-//Export routes for server.js to use.
-module.exports = router;
+ router.put("/burgers/update", function(req, res) {
+     db.burger.update({
+         devoured: true
+     }, {
+         where: {
+             id: req.body.burger_id
+         }
+     }).then(function(dbBurger) {
+
+         res.redirect('/');
+     });
+ });
+
+
+ //Export routes for server.js to use.
+ module.exports = router;
